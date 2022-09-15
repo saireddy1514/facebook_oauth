4e537e700814e42284ccbae15332093678d4da15
saireddy.dvs <saireddy.dvs@holosuit.com>
2022-09-15 10:24:38 +0530

    testing

---
+++ routes/public/auth.js
@@ -0,0 +1,110 @@
+const passport=require('passport')
+const getIP = require('ipware')().get_ip;
+const router=require('express').Router();
+var requestIp = require('request-ip');
+const User=require('../../models/User');
+const jwt=require('jsonwebtoken');
+const sessionStorage = require('sessionstorage-for-nodejs');
+router.get('/login',(req,res)=>
+{
+    res.send('index')
+})
+//Normal authectication
+//This not yet done it is for General users who not have social media accounts
+router.post('/verify',(req,res)=>
+{
+    User.findOne({email:req.email})
+    .then((person)=>
+    {
+        if(person)
+        {
+            bcrypt.compare(req.body.password,person.password)
+            .then(
+                value=>
+                {
+                    console.log(value)
+                    if(value)
+                    {
+                        console.log("dadasdad");
+                        const payload={
+                            id:person.id,
+                            email:person.email
+                        }
+                        jwt.sign(
+                            payload,
+                            key,
+                            {expiresIn:3600},
+                            (err,token)=>
+                            {
+                                if(err)
+                                {
+                                    console.log(err);
+                                }
+                                else
+                                {
+                                  
+                                    res.cookie('token',token)
+                                    return res.render('home');
+                                }
+                            }
+
+                        )
+                    }
+                }).catch(err=>console.log(err))
+        }
+        else
+        {
+                return res.redirect('/');
+        }
+    })
+    .catch(err=>console.log(err))
+})
+router.get('/facebook',passport.authenticate('facebook',{scope:'email'}))
+//Facebook authentication and setting the jwt token in cookie
+router.get('/facebook/secrets',passport.authenticate('facebook'),(req,res)=>
+{
+
+         var ip = req.socket.remoteAddress
+         var clientIp = requestIp.getClientIp(req);
+        // console.log(req);
+            //So here req.user.id is FbId which is assging by Facebook strategy don't confuse with mongoDb-id
+        User.findOne({FbId:req.user.id})
+        .then((person)=>
+        {
+           
+                if(person)
+                {
+                    const payload={
+                        id:person._id,
+                        Fb:person.FbId
+                    }
+                    const key="login$";
+                    jwt.sign(
+                        payload,
+                        key,
+                        {expiresIn:3600},
+                        (err,token)=>
+                        {
+                            if(err)
+                            {
+                                console.log(err);
+
+                            }
+                            else
+                            {
+                                 res.cookie("token",token);
+                                return res.render("home",{user:req.user,Ip:clientIp});
+                            }
+                        }
+            
+                    )
+                }
+        })
+        .catch((err)=>console.log(err));
+})
+router.get('/holoworld',passport.authenticate('jwt',{session:false}),(req,res)=>
+{
+    //console.log(req);
+    return res.render('holoworld');
+})
+module.exports=router;
---
+++ strategy/facebook-strat.js
@@ -0,0 +1,58 @@
+const FacebookStrategy=require('passport-facebook');
+const User=require('../models/User');
+const bcrypt=require('bcryptjs')
+opts=
+{
+    clientID: 1730412367304237,
+    clientSecret: '3e5ca655807bcb4a09af5c3966f3fd50',
+    callbackURL: "https://127.0.0.1:4000/auth/facebook/secrets",
+    profileFields: ['id', 'displayName', 'photos', 'email']
+    // ,'user_mobile_phone'
+}
+module.exports=passport=>{passport.use(new FacebookStrategy(opts,
+ (accessToken,RefershToken,profile,done)=>
+{
+//   console.log(profile);
+console.log(accessToken)
+  User.findOne({username:profile.displayName})
+  .then((user)=>
+  {
+
+      if(user)
+      {
+          console.log('User already existed');
+      }
+      else
+      {
+          let ee=null;
+          ee=profile.emails[0].value;
+         const newUser= new User(
+              {
+                  username:profile.displayName,
+                  FbId:profile.id,
+                  email:ee,
+                  password:'Fb123'
+              })
+              bcrypt.genSalt(10,(err,slat)=>
+              {
+                bcrypt.hash(newUser.password,slat,(err,hash)=>
+                {
+                    if(err)
+                            {
+                                res.redirect("/");
+                            }
+                            else
+                            {
+                                newUser.password=hash;
+                                newUser.save()
+                                .then((newUser)=>{console.log(`User was succesfully saved`)})
+                                .catch((err)=>console.log(err))
+                            }
+                })
+              })
+
+      }
+  })
+  .catch((err)=>console.log(err));
+  return done(null,profile);
+}))};
